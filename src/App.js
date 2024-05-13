10a9f603f97fd2fcd89100c6f06f654efa12768f
JulioManoel <juliobonow@gmail.com>
2024-05-12 23:38:31 -0300

    update

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default () => {
                 {/* General */}
                 {/* <Stack.Screen name='Test' component={test}/>       */}
                 <Stack.Screen name='Welcome' component={Welcome}/>
-                {/* <Stack.Screen name='LoginScreen' component={LoginScreen}/> */}
+                <Stack.Screen name='LoginScreen' component={LoginScreen}/>
 
                 {/* Student */}
                 {/* <Stack.Screen name='HomeStudent' component={HomeStudentScreen}/>
