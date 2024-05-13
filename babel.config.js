2a2d5763e56baf7e3864e7ce7922f044e1853af9
JulioManoel <juliobonow@gmail.com>
2024-05-12 22:17:49 -0300

    update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  presets: [
-    'module:@react-native/babel-preset',
-    'module:react-native-dotenv'
-  ],
+  presets: ['module:@react-native/babel-preset'],
   plugins: ['react-native-reanimated/plugin'],
-};
+}
