5eb26f5d38f32e1264597c24c4e0a3cd971b8e8a
JulioManoel <juliobonow@gmail.com>
2024-05-13 00:01:07 -0300

    update

--- src/components/atoms/TextInputMail.js
+++ src/components/atoms/TextInputMail.js
@@ -4,12 +4,12 @@ import SvgMailIcon from '../../../assets/svg/icons/SvgMailIcon'
 import SvgCheckIcon from '../../../assets/svg/icons/SvgCheckIcon'
 
 export default props => {
-    const { placeholder } = props;
-    const [email, setEmail] = useState('');
+    // const { placeholder } = props.placeholder
+    const [email, setEmail] = useState('')
 
     const isValidEmail = (email) => {
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return re.test(String(email).toLowerCase())
     }
 
     return (
@@ -17,7 +17,7 @@ export default props => {
             <View style={style.inputContainer}>
                 <SvgMailIcon/>
                 <TextInput 
-                    placeholder=' Email institucional'
+                    placeholder={props.placeholder}
                     onChangeText={text => setEmail(text)}
                     value={email}
                 />
--- src/controllers/BaseController.js
+++ src/controllers/BaseController.js
@@ -1,9 +1,45 @@
 export class BaseController {
   constructor() {
-    this.baseURL = process.env.BASE_URL
+    this.baseURL = 'http://pitesla.juliomanoel.com/api'
   }
 
-  async get() {
-    console.log(this.baseURL)
+  async get(endpoint, params = {}) {
+    try {
+      const response = await axios.get(`${this.baseURL}/${endpoint}`, { params })
+      return response.data
+    } catch (error) {
+      console.error('Erro ao realizar requisição GET:', error)
+      throw error
+    }
+  }
+
+  async post(endpoint, data = {}) {
+    try {
+      const response = await axios.post(`${this.baseURL}/${endpoint}`, data)
+      return response.data
+    } catch (error) {
+      console.error('Erro ao realizar requisição POST:', error)
+      throw error
+    }
+  }
+
+  async put(endpoint, data = {}) {
+    try {
+      const response = await axios.put(`${this.baseURL}/${endpoint}`, data)
+      return response.data
+    } catch (error) {
+      console.error('Erro ao realizar requisição PUT:', error)
+      throw error
+    }
+  }
+
+  async delete(endpoint) {
+    try {
+      const response = await axios.delete(`${this.baseURL}/${endpoint}`)
+      return response.data
+    } catch (error) {
+      console.error('Erro ao realizar requisição DELETE:', error)
+      throw error
+    }
   }
 }
