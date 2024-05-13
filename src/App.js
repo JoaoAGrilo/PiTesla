0308bfc1aab143395ad2502570ef3bac9c1d752c
JulioManoel <juliobonow@gmail.com>
2024-05-12 23:36:17 -0300

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 // import test from './views/test'
 import Welcome from './views/WelcomeScreen'
-// import LoginScreen from './views/LoginScreen'
+import LoginScreen from './views/LoginScreen'
 
 // import HomeStudentScreen from './views/student/HomeStudentScreen'
 // import SubjectStudentScreen from './views/student/SubjectStudentScreen'
